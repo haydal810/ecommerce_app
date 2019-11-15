82477d3b414ad1800b7b2e1b47209af305906c08
David <haydal.tmps@gmail.com>
2019-11-15 09:23:14 +0000

    change to settings.py, added to list

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -67,7 +67,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
-                'cart.contexts.cart_contents'
+                'cart.contexts.cart_contents',
+                'django.contrib.staticfiles',
             ],
         },
     },
